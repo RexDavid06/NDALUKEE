1593e1ad2900912d33b00c8b857bca639f412394
rexdavid06 <rhexmilia06@gmail.com>
2026-02-18 17:58:26 +0100

    modified domain name

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['xcg.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ndalukee-p39t.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
